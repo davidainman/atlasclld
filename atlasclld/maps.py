ae0947f0636216aa2722ad21f83f23bec146f0f4
davidainman <havennah@gmail.com>
2025-02-03 18:41:30 +0100

    Making map dots larger in size

--- atlasclld/maps.py
+++ atlasclld/maps.py
@@ -9,24 +9,20 @@ class FeatureMap(ParameterMap):
         ParameterMap.__init__(self, ctx, req, eid=eid)
 
     def get_options(self):
-        if (self.ctx.datatype == "multi-valued" or self.ctx.datatype == "frequency"):
-            return {'max_zoom': 6, 'icon_size': 35}
-        else:
-            return {'max_zoom': 6, 'icon_size': 17}
+        return {'max_zoom': 6, 'icon_size': 20}
+        #if (self.ctx.datatype == "multi-valued" or self.ctx.datatype == "frequency"):
+        #    return {'max_zoom': 6, 'icon_size': 20}
+        #else:
+        #    return {'max_zoom': 6, 'icon_size': 20}
 
     def get_layers(self):
-        #if self.ctx.multivalued:
         yield Layer(
             self.ctx.id,
             self.ctx.name,
             GeoJsonFeature(self.ctx).render(self.ctx, self.req, dump=False)
         )
-        # else:
-        #     for layer in super(FeatureMap, self).get_layers():
-        #         yield layer
 
     def get_legends(self):
-        #if self.ctx.multivalued:
         def value_li(de):
             return HTML.label(
                 map_marker_img(self.req, de),
@@ -34,11 +30,6 @@ class FeatureMap(ParameterMap):
                 style='margin-left: 1em; margin-right: 1em;')
 
         yield Legend(self, 'values', map(value_li, self.ctx.domain), label='Legend')
-
-        # for legend in super(FeatureMap, self).get_legends():
-        #     yield legend
-        #
-        # yield FilterLegend(self, 'WILLSEE?', col=self.col, dt=self.dt)
 
 
 def includeme(config):
