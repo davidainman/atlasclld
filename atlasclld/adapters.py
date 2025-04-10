e30254071bda87d0651ce8e555a31a561f2b1cfd
davidainman <havennah@gmail.com>
2025-04-10 13:33:20 +0200

    Removing redundant code from GeoGsonFeature now that sizes are fixed.

--- atlasclld/adapters.py
+++ atlasclld/adapters.py
@@ -19,17 +19,9 @@ class GeoJsonFeature(GeoJsonParameter):
             'values': list(valueset.values),
             'label': valueset.language.name }
         if valueset.parameter.datatype == 'integer':
-            vals = sorted(set([vs.values[0].value for vs in ctx.valuesets if vs.values[0].value is not None]))
-            min_val = 2.7
-            scale_factor = 8 / len(vals)
-            size_dict = dict()
-            for i in range(0, len(vals)):
-                size_dict[vals[i]] = min_val + scale_factor * i
-            size_dict['NA'] = 2.6
-            size_dict['?'] = 2.6
             val = valueset.values[0]
             if not val.domainelement.jsondata.get('icon'):
-                res['icon'] = data_url(ATLAsPie([1], ['#ffffff'], width=size_dict[val.value], opacity=0.0, stroke_circle=True))
+                res['icon'] = data_url(ATLAsPie([1], ['#ffffff'], width=2.6, opacity=0.0, stroke_circle=True))
             if val.domainelement and val.domainelement.jsondata.get('icon'):
                 res['icon'] = data_url(ATLAsPie([1], [val.domainelement.jsondata['icon']], width=25, opacity=1, stroke_circle=True))
         if valueset.parameter.datatype != 'integer' and not valueset.values[0].domainelement.jsondata.get('icon'):
